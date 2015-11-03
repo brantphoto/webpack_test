b3beed6396904e2280841ba9856d5e9ec5e1d2af
brantphoto <brant.barger@gmail.com>
2015-11-03 13:56:26 -0800

    extracted init logic to module

--- entry.js
+++ entry.js
@@ -1,65 +1,8 @@
 require("!style!css!./style.css");
 document.write(require("./content.js"));
-var el = document.getElementById('example');
 
-el.addEventListener('mouseover', function(event) {
-
-  event.stopImmediatePropagation();
-  var padding = el.style.padding;
-
-  if (typeof padding === 'string') {
-    console.log(padding);
-    padding = parseInt(padding, 10) || 25;
-  }
-
-  console.log(padding);
-  if (padding <= 50) {
-    animateTitleGrow(el);
-  }
-
-  function animateTitleGrow(element) {
-
-    function frame() {
-      padding++;
-      element.style.padding = padding + 'px';
-      if (padding >= 50) {
-        clearInterval(id);
-      }
-    }
-
-    var id = setInterval(frame, 5);
-
-  }
-
-});
-
-el.addEventListener('mouseleave', function(event) {
-
-  event.stopImmediatePropagation();
-  var padding = el.style.padding;
-
-  if (typeof padding === 'string') {
-    padding = parseInt(padding, 10) || 60;
-  }
-
-  if (padding >= 26) {
-    animateTitleShrink(el);
-  }
-
-  function animateTitleShrink(element) {
-
-    function frame() {
-      padding--;
-      element.style.padding = padding + 'px';
-
-      if (padding <= 25) {
-        clearInterval(id);
-      }
-    }
-
-    var id = setInterval(frame, 5);
-  }
-});
+var init = require('./scripts/init');
+init();
 
 
 
---
+++ scripts/init.js
@@ -0,0 +1,62 @@
+function init() {
+  var el = document.getElementById('example');
+
+  el.addEventListener('mouseover', function(event) {
+
+    event.stopImmediatePropagation();
+    var padding = el.style.padding;
+
+    if (typeof padding === 'string') {
+      padding = parseInt(padding, 10) || 25;
+    }
+
+    if (padding <= 50) {
+      animateTitleGrow(el);
+    }
+
+    function animateTitleGrow(element) {
+
+      function frame() {
+        padding++;
+        element.style.padding = padding + 'px';
+        if (padding >= 50) {
+          clearInterval(id);
+        }
+      }
+
+      var id = setInterval(frame, 5);
+
+    }
+
+  });
+
+  el.addEventListener('mouseleave', function(event) {
+
+    event.stopImmediatePropagation();
+    var padding = el.style.padding;
+
+    if (typeof padding === 'string') {
+      padding = parseInt(padding, 10) || 60;
+    }
+
+    if (padding >= 26) {
+      animateTitleShrink(el);
+    }
+
+    function animateTitleShrink(element) {
+
+      function frame() {
+        padding--;
+        element.style.padding = padding + 'px';
+
+        if (padding <= 25) {
+          clearInterval(id);
+        }
+      }
+
+      var id = setInterval(frame, 5);
+    }
+  });
+}
+
+module.exports = init;
